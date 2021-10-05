20f9a77f2a56ef1b756a8258d82699897cb537fb
Amarnath Kumar Choudhary <87637319+amarnath5789@users.noreply.github.com>
2021-10-05 18:06:39 +0530

    change style

--- index.php
+++ index.php
@@ -5,16 +5,21 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
     <title>Document</title>
 </head>
 
 <body>
-    <div class="container-fluid pt-4">
+    <nav class="navbar navbar-expand-md fixed-top">
+        <div class="container">
+            <a href="https://cppsecrets.com/"><img src="logo.png" width="150" height="60"></a>
+        </div>
+    </nav>
+
+    <div class="container-fluid main-content">
         <div class="row">
-            <div class="col-sm-6 col-md-2 order-md-0 px-5 left_side">
+            <div class="col-sm-6 col-md-2 order-md-0 px-5">
                 <ul class="list-unstyled text-center small">
                     <li class="list-item my-1 py-4 border rounded wpm">
                         WPM<span class="d-block display-4 curr_wpm">0</span>
@@ -32,36 +37,28 @@
                             Start the one-minute Typing speed test and find out how fast can
                             you type in real world!
                         </p>
-                        <div class="alert alert-danger" role="alert"
-                            style="background-color: #f9bfc400; color: #c30012;">Just start typing and you can use
+                        <div class="alert alert-danger" role="alert" style="background-color: #f9bfc400; color: #c30012;">Just start typing and you can use
                             <b>backspace</b> to
                             correct your mistakes. Your mistakes will be marked in <u>Red</u> color and shown below the
                             writing box. Good luck!
                         </div>
                     </div>
                     <div class="quote my-4 rounded"> Click on the area below to start typing</div>
-                    <textarea class="p-4 my-4 bg-dark text-light rounded lead input_area"
-                        placeholder="Start typing here..." onfocus="startGame()"
-                        oninput="processCurrentText()"></textarea>
+                    <textarea class="p-4 my-4 bg-dark text-light rounded lead input_area" placeholder="Start typing here..." onfocus="startGame()" oninput="processCurrentText()"></textarea>
 
-                    <button class="btn btn-outline-success restart_btn" id="editbtn"
-                        onclick="resetValues()">
+                    <button class="btn btn-outline-success restart_btn" id="editbtn" onclick="resetValues()">
                         Restart</button><span class="btn-circle-animation"></span>
 
                     <hr class="my-4" />
                     <div class="mb-5">
                         <h6 class="py-2">Average Typing Speeds</h6>
                         <div class="d-flex text-white meter-gauge">
-                            <span class="col" style="background: rgb(235, 72, 65)">0 - 20 Slow</span><span class="col"
-                                style="background: rgb(244, 136, 71)">20 - 40 Average</span><span class="col"
-                                style="background: rgb(255, 200, 74)">40 - 60 Fast</span><span class="col"
-                                style="background: rgb(166, 195, 76)">60 - 80 Professional</span><span class="col"
-                                style="background: rgb(78, 192, 78)">80 - 100+ Top</span>
+                            <span class="col" style="background: rgb(235, 72, 65)">0 - 20 Slow</span><span class="col" style="background: rgb(244, 136, 71)">20 - 40 Average</span><span class="col" style="background: rgb(255, 200, 74)">40 - 60 Fast</span><span class="col" style="background: rgb(166, 195, 76)">60 - 80 Professional</span><span class="col" style="background: rgb(78, 192, 78)">80 - 100+ Top</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-2 order-md-2 px-5">
+            <div class="col-sm-6 col-md-2 order-md-2 px-5 ">
                 <ul class="list-unstyled text-center small">
                     <li class="list-item my-1 py-4 border rounded">
                         Timers<span class="d-block display-4 curr_time">60</span>
@@ -77,9 +74,7 @@
         </div>
     </div>
     <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
 
 </html>
